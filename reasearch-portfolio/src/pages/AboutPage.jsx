98430d833417b5ac72a12e041093c245a406a1ef
sydsedloff <ssedloff@knights.ucf.edu>
2023-11-14 09:23:57 -0500

    STYLE: about page

--- reasearch-portfolio/src/pages/AboutPage.jsx
+++ reasearch-portfolio/src/pages/AboutPage.jsx
@@ -1,7 +1,19 @@
 function AboutPage() {
   return (
     <div className="App">
-      <p>This is some text for the about page</p>
+      <img
+        src="https://placehold.co/300x200"
+        alt="Mariah Profile"
+        className="profile-picture"
+      ></img>
+      <h1>Here's a little about me.</h1>
+      <p>
+        Put more stuff here, like a summary of who you are, what kind of work
+        you do, a bit about your background, and maybe give a little of your
+        personality.
+      </p>
+      <p>And here's another paragraph</p>
+      <button>Let's Work Together</button>
     </div>
   );
 }
