93fa02448ec3f0f9b3fb11a6bd6b8fcf6d0a626a
sydsedloff <ssedloff@knights.ucf.edu>
2023-11-14 19:42:14 -0500

    STYLE: contact page form

--- reasearch-portfolio/src/pages/ContactPage.jsx
+++ reasearch-portfolio/src/pages/ContactPage.jsx
@@ -1,7 +1,33 @@
 function ContactPage() {
   return (
-    <div>
-      <p>This is some text for the contact page</p>
+    <div className="App">
+      <h1>Let's work together</h1>
+      <form>
+        <label>
+          Email
+          <input type="text" name="email" placeholder="email@company.com" />
+        </label>
+        <label>
+          Full Name
+          <input type="text" name="fullName" placeholder="John Doe" />
+        </label>
+        <label>
+          Company Name
+          <input type="text" name="companyName" placeholder="Company Inc." />
+        </label>
+        <label>
+          Message
+          <input
+            type="text"
+            name="message"
+            placeholder="Want to collaborate? Tell me a bit more about yourself"
+            rows
+          />
+        </label>
+        <button type="submit" value="Submit" className="button">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
