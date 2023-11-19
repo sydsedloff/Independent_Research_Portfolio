cc2480b4f838af8dbf36adf9643f93afc0ae6859
sydsedloff <ssedloff@knights.ucf.edu>
2023-11-18 20:04:42 -0500

    STYLE: project images

--- research-portfolio/src/pages/ProjectPage.jsx
+++ research-portfolio/src/pages/ProjectPage.jsx
@@ -9,6 +9,15 @@ function ProjectPage() {
         <div className="filter-buttons">2d animation</div>
         <div className="filter-buttons">3d modeling</div>
       </div>
+      <div>
+        {/* PLACEHOLDER. THIS WILL BE A REACT COMPONENT */}
+        <img
+          src="https://placehold.co/300x200"
+          alt="Project"
+          className="project-pictures"
+        ></img>
+        <p>Project Name</p>
+      </div>
     </div>
   );
 }
