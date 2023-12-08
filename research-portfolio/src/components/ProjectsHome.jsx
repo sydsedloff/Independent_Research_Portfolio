6a1a75a59379dd75d4cebc5fcddcc130e4fcd3e5
Jessie Jalca <jessiejalca@gmail.com>
2023-12-07 23:55:07 -0500

    FEAT: only render first 3 projects in home

--- research-portfolio/src/components/ProjectsHome.jsx
+++ research-portfolio/src/components/ProjectsHome.jsx
@@ -4,25 +4,27 @@ import data from "../assets/data"
 export default function ProjectsHome() {
   return (
     <div className="home-grid">
-      {data.map((item) => (
-        <div key={item.id}>
-          <div style={{ display: "inline-block" }}>
-            <img
-              src={item.images[0].image1}
-              alt="Mariah Profile"
-              className="project-pictures"></img>
-          </div>
+      {data
+        .filter((item, key) => (key < 3 ? (item, key) : []))
+        .map((item, key) => (
+          <div key={key}>
+            <div>
+              <img
+                src={item.images[0].image1}
+                alt="Mariah Profile"
+                className="project-pictures"></img>
+            </div>
 
-          <div
-            style={{
-              display: "inline-block",
-              verticalAlign: "top",
-            }}>
-            <h2>{item.title}</h2>
-            <p>{item.description}</p>
+            <div
+              style={{
+                display: "inline-block",
+                verticalAlign: "top",
+              }}>
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   )
 }
--- research-portfolio/src/components/ProjectsProject.jsx
+++ research-portfolio/src/components/ProjectsProject.jsx
@@ -1,21 +1,20 @@
-import "../App.css";
-import data from "../assets/data";
+import "../App.css"
+import data from "../assets/data"
 
 export default function ProjectsProject() {
   return (
     <div className="project-grid">
-      {data.map((item) => (
-        <div key={item.id}>
-          <div style={{ display: "inline-block" }}>
+      {data.map((item, key) => (
+        <div key={key}>
+          <div>
             <img
               src={item.images[0].image1}
               alt="Mariah Profile"
-              className="project-pictures"
-            ></img>
+              className="project-pictures"></img>
             <h3>{item.title}</h3>
           </div>
         </div>
       ))}
     </div>
-  );
+  )
 }
