389aa40a7d1915f90a791aa02d7e662b7ebbe021
Jessie Jalca <jessiejalca@gmail.com>
2023-12-08 11:36:43 -0500

    FEAT: projects in project page will list title in console on click

--- research-portfolio/src/components/ProjectsProject.jsx
+++ research-portfolio/src/components/ProjectsProject.jsx
@@ -1,20 +1,32 @@
 import "../App.css"
 import data from "../assets/data"
+import ProjectModal from "./ProjectModal"
 
 export default function ProjectsProject() {
+  const openProject = (item) => {
+    console.log(item.title)
+  }
+
   return (
-    <div className="project-grid">
-      {data.map((item, key) => (
-        <div key={key}>
-          <div>
-            <img
-              src={item.images[0]}
-              alt="Mariah Profile"
-              className="project-pictures"></img>
-            <h3>{item.title}</h3>
+    <>
+      <ProjectModal />
+      <div className="project-grid">
+        {data.map((item, key) => (
+          <div
+            key={key}
+            onClick={() => {
+              openProject(item)
+            }}>
+            <div>
+              <img
+                src={item.images[0]}
+                alt="Mariah Profile"
+                className="project-pictures"></img>
+              <h3>{item.title}</h3>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   )
 }
