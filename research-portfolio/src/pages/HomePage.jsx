4ce5f2c7b72bdcce0ea0a9a82cb00fbbe62ceabc
Jessie Jalca <jessiejalca@gmail.com>
2023-12-07 22:56:04 -0500

    CONTENT: add profile image

--- research-portfolio/src/pages/HomePage.jsx
+++ research-portfolio/src/pages/HomePage.jsx
@@ -1,15 +1,15 @@
 import "../App.css"
 import "../components/ProjectsHome"
 import ProjectsHome from "../components/ProjectsHome"
+import profileImg from "../assets/images/MariahProfile.jpg"
 
 function HomePage() {
   return (
     <>
       <div className="Home-Heading">
-        <img
-          src="https://placehold.co/150x150"
-          alt="Mariah Profile"
-          className="profile-picture-circle"></img>
+        <div className="profile-picture-circle">
+          <img src={profileImg} alt="Mariah Profile"></img>
+        </div>
         <div className="Page-Heading">
           <h1>Hi, I'm Mariah. I'm an animation designer.</h1>
           <a href="/about">Learn more about me </a>
