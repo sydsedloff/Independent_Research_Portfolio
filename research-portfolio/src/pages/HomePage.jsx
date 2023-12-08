51455b853e739952d903c5d633871cc7fbaa47dd
Jessie Jalca <jessiejalca@gmail.com>
2023-12-07 20:25:33 -0500

    STYLE: adjust nav & header typography, layout

--- research-portfolio/src/pages/HomePage.jsx
+++ research-portfolio/src/pages/HomePage.jsx
@@ -1,34 +1,25 @@
-import "../App.css";
-import "../components/ProjectsHome";
-import ProjectsHome from "../components/ProjectsHome";
+import "../App.css"
+import "../components/ProjectsHome"
+import ProjectsHome from "../components/ProjectsHome"
 
 function HomePage() {
   return (
     <div className="App">
-      <div style={{ display: "flex", marginBottom: "2rem" }}>
+      <main>
         <div>
           <img
             src="https://placehold.co/150x150"
             alt="Mariah Profile"
-            className="profile-picture-circle"
-          ></img>
+            className="profile-picture-circle"></img>
         </div>
-        <div style={{ textAlign: "left" }}>
-          <h1 style={{ paddingLeft: "1rem" }}>
-            Hi, I'm Mariah. I'm an animation designer.
-          </h1>
-          <a
-            href="/about"
-            style={{ paddingLeft: "1rem", textDecoration: "underline" }}
-          >
-            Learn more about me{" "}
-          </a>
+        <div>
+          <h1>Hi, I'm Mariah. I'm an animation designer.</h1>
+          <a href="/about">Learn more about me </a>
         </div>
-      </div>
-
-      <ProjectsHome />
+        <ProjectsHome />
+      </main>
     </div>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
--- research-portfolio/src/pages/Navigation.jsx
+++ research-portfolio/src/pages/Navigation.jsx
@@ -3,29 +3,29 @@ import { Outlet, Link } from "react-router-dom"
 function NavBar() {
   return (
     <>
-      <div>
-        <h1 className="Header">
-          <div className="Header-text">Mariah Aviles</div>
-        </h1>
-      </div>
-      <nav className="Nav">
-        <ul>
-          <li>
-            <Link to="/Home">Home</Link>
-          </li>
-          <li>
-            <Link to="/Projects">Projects</Link>
-          </li>
-          <li>
-            <Link to="/About">About</Link>
-          </li>
-          <li>
-            <Link to="/Contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      <header>
+        <Link to="/Home">Mariah Aviles</Link>
+      </header>
+      <div className="Container">
+        <nav className="Nav">
+          <ul>
+            <li>
+              <Link to="/Home">Home</Link>
+            </li>
+            <li>
+              <Link to="/Projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+            <li>
+              <Link to="/Contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <Outlet />
+        <Outlet />
+      </div>
     </>
   )
 }
