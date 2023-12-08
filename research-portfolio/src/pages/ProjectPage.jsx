675fa4ceda9c455903997fd6a55ada9cf56ab4be
Jessie Jalca <jessiejalca@gmail.com>
2023-12-08 12:52:20 -0500

    STYLE: add selected tag design based on filter

--- research-portfolio/src/pages/ProjectPage.jsx
+++ research-portfolio/src/pages/ProjectPage.jsx
@@ -13,28 +13,36 @@ function ProjectPage() {
       <h1>Projects</h1>
       <div className="filter-container">
         <button
-          className="filter-buttons"
+          className={`filter-buttons ${
+            filterTag === "all" ? "filter-buttons-selected" : ""
+          }`}
           onClick={() => {
             switchFilter("all")
           }}>
           all
         </button>
         <button
-          className="filter-buttons"
+          className={`filter-buttons ${
+            filterTag === "motion design" ? "filter-buttons-selected" : ""
+          }`}
           onClick={() => {
             switchFilter("motion design")
           }}>
           motion design
         </button>
         <button
-          className="filter-buttons"
+          className={`filter-buttons ${
+            filterTag === "animation" ? "filter-buttons-selected" : ""
+          }`}
           onClick={() => {
             switchFilter("animation")
           }}>
           animation
         </button>
         <button
-          className="filter-buttons"
+          className={`filter-buttons ${
+            filterTag === "3d modeling" ? "filter-buttons-selected" : ""
+          }`}
           onClick={() => {
             switchFilter("3d modeling")
           }}>
