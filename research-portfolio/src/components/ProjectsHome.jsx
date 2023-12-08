a5133765f968649c7b1f87d65da8fd75311155b1
Jessie Jalca <jessiejalca@gmail.com>
2023-12-07 20:48:07 -0500

    FIX: home layout

--- research-portfolio/src/components/ProjectsHome.jsx
+++ research-portfolio/src/components/ProjectsHome.jsx
@@ -1,5 +1,5 @@
-import "../App.css";
-import data from "../assets/data";
+import "../App.css"
+import data from "../assets/data"
 
 export default function ProjectsHome() {
   return (
@@ -10,24 +10,19 @@ export default function ProjectsHome() {
             <img
               src={item.images[0].image1}
               alt="Mariah Profile"
-              className="project-pictures"
-            ></img>
+              className="project-pictures"></img>
           </div>
 
           <div
             style={{
-              textAlign: "left",
               display: "inline-block",
               verticalAlign: "top",
-            }}
-          >
+            }}>
             <h2>{item.title}</h2>
-            <p style={{ textAlign: "left", padding: "0px", maxWidth: "40rem" }}>
-              {item.description}
-            </p>
+            <p>{item.description}</p>
           </div>
         </div>
       ))}
     </div>
-  );
+  )
 }
--- research-portfolio/src/pages/HomePage.jsx
+++ research-portfolio/src/pages/HomePage.jsx
@@ -6,15 +6,15 @@ function HomePage() {
   return (
     <div className="App">
       <main>
-        <div>
+        <div className="Home-Heading">
           <img
             src="https://placehold.co/150x150"
             alt="Mariah Profile"
             className="profile-picture-circle"></img>
-        </div>
-        <div>
-          <h1>Hi, I'm Mariah. I'm an animation designer.</h1>
-          <a href="/about">Learn more about me </a>
+          <div className="Main-Heading">
+            <h1>Hi, I'm Mariah. I'm an animation designer.</h1>
+            <a href="/about">Learn more about me </a>
+          </div>
         </div>
         <ProjectsHome />
       </main>
