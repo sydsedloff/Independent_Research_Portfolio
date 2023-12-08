3041b197fb8f36100ebdcd48af9486ac2fae7f23
Jessie Jalca <jessiejalca@gmail.com>
2023-12-08 00:21:49 -0500

    FIX: correct first 3 projects rendered on home page

--- research-portfolio/src/components/ProjectsHome.jsx
+++ research-portfolio/src/components/ProjectsHome.jsx
@@ -2,29 +2,29 @@ import "../App.css"
 import data from "../assets/data"
 
 export default function ProjectsHome() {
+  const truncData = data.slice(0, 3)
+
   return (
     <div className="home-grid">
-      {data
-        .filter((item, key) => (key < 3 ? (item, key) : []))
-        .map((item, key) => (
-          <div key={key}>
-            <div>
-              <img
-                src={item.images[0].image1}
-                alt="Mariah Profile"
-                className="project-pictures"></img>
-            </div>
-
-            <div
-              style={{
-                display: "inline-block",
-                verticalAlign: "top",
-              }}>
-              <h2>{item.title}</h2>
-              <p>{item.description}</p>
-            </div>
+      {truncData.map((item, key) => (
+        <div key={key}>
+          <div>
+            <img
+              src={item.images[0].image1}
+              alt="Mariah Profile"
+              className="project-pictures"></img>
           </div>
-        ))}
+
+          <div
+            style={{
+              display: "inline-block",
+              verticalAlign: "top",
+            }}>
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
